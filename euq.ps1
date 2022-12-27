2fb8ee0f16c1a6a32b459fc26f5a9b23fa4696cc
C_R <caravanreversal@proton.me>
2022-12-27 12:13:30 +0100

    0.2.4 Added license related

--- euq.ps1
+++ euq.ps1
@@ -1,4 +1,10 @@
-﻿# CONSTANT
+﻿# Copyright (c) 2022, caravanreversal
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. 
+
+
+# CONSTANT
 $historyPath = ".\history.txt"
 
 # CLI
